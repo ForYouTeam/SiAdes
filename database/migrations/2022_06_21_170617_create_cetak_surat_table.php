4dfae586e3de12123243743ef274672aae8320f7
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-21 17:10:07 +0800

    migration cetak surat

---
+++ database/migrations/2022_06_21_170617_create_cetak_surat_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCetakSuratTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('cetak_surat', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_penduduk')->constrained('penduduk');
+            $table->string('jenis_surat');
+            $table->foreignId('nama_ayah')->constrained('penduduk')->nullable();
+            $table->foreignId('nama_ibu')->constrained('penduduk')->nullable();
+            $table->foreignId('id_ctksuratkematian')->constrained('cetak_surat_kematian')->nullable();
+            $table->string('ttd');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('cetak_surat');
+    }
+}
