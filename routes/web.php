93150da084ba9fbc289e10cf70a91da4d6466d59
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-21 21:56:03 +0800

    route update data penduduk

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\CMS\PendudukController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('example');
+});
+
+Route::prefix('penduduk')->group(function () {
+    Route::get('/',[PendudukController::class, 'getAllPenduduk']);
+    Route::post('/', [PendudukController::class, 'createPenduduk']);
+    Route::get('/{id}', [PendudukController::class, 'getPendudukById']);
+    Route::patch('/{id}', [PendudukController::class, 'updateDataPenduduk']);
 });
