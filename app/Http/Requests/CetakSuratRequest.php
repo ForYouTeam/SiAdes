3c19085b2883433d8a478cb3bcdf78c809c57f86
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-21 21:13:31 +0800

    modified request

--- app/Http/Requests/CetakSuratRequest.php
+++ app/Http/Requests/CetakSuratRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class CetakSuratRequest extends FormRequest
 {
@@ -21,5 +23,20 @@ class CetakSuratRequest extends FormRequest
             'id_ctksuratkematian' => 'required',
             'ttd' => 'required',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'response' => array(
+                'icon' => 'error',
+                'title' => 'Validasi Gagal',
+                'message' => 'Data yang di input tidak tervalidasi',
+            ),
+            'errors' => array(
+                'length' => count($validator->errors()),
+                'data' => $validator->errors()
+            ),
+        ], 422));
     }
 }
