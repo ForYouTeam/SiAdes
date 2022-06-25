230674588a65538bb33b588a27dc2a935ff9ae5c
IPKIN9 <irwandirini@gmail.com>
2022-06-25 16:18:15 +0800

    Path

--- app/Http/Controllers/CMS/CetakSuratController.php
+++ app/Http/Controllers/CMS/CetakSuratController.php
@@ -133,7 +133,7 @@ class CetakSuratController extends Controller
     {
         $allData = CetakSuratModel::whereId($id)->with('pendudukRole', 'cetaksuratKematianRole', 'ttdRole:id,nama,jabatan', 'ayahRole', 'ibuRole')->first();
 
-        $allData['path'] = public_path('logoLuwuTimur.png');
+        $allData['path'] = env('APP_URL') . '/logoLuwuTimur.png';
         $allData['dateNow'] = Carbon::now()->isoFormat('D MMMM Y');
 
         switch ($allData['jenis_surat']) {
