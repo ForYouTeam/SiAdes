f359a3f20e2eeae3e929a93b26d25f496200ffd3
IPKIN9 <irwandirini@gmail.com>
2022-06-25 15:29:28 +0800

    Fix console log

--- resources/views/Auth/Akun.blade.php
+++ resources/views/Auth/Akun.blade.php
@@ -127,7 +127,6 @@
                 method: "POST",
                 data: data,
                 success: function(result) {
-
                     Swal.fire({
                         title: result.response.title,
                         text: result.response.message,
