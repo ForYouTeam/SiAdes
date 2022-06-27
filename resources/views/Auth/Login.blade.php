9c83fd69cc1c09b0d24c00b49ce24847d6bf297a
IPKIN9 <irwandirini@gmail.com>
2022-06-27 08:56:23 +0800

    Add logout link

--- resources/views/Auth/Login.blade.php
+++ resources/views/Auth/Login.blade.php
@@ -5,7 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Spica Admin</title>
+    <title>{{ config('app.name') }}</title>
 
     <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}">
